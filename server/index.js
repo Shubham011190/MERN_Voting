595a91b163721111f727b6aa5dd63c0cdcc70fbf
Shubham Singhal <shubham.011190@gmail.com>
2020-12-12 14:13:02 +0530

    Server started with error path

--- server/index.js
+++ server/index.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+const PORT = 5000;
+app.get('/', (req, res) => {
+    res.json({ hello: "world" });
+});
+
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        err: err.message || "Something went wrong!"
+    })
+});
+
 app.listen(PORT, console.log(`Server started at port ${PORT}`));
