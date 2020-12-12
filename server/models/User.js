d258d1abd1554527a8d5cd801d096bc485f5636f
Shubham Singhal <shubham.011190@gmail.com>
2020-12-12 21:50:38 +0530

    Login

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -10,7 +11,28 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    created: {
+        type: Date,
+        default: Date.now,
+    },
     polls: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Poll' }]
 });
+userSchema.pre('save', async function (next) {
+    try {
+        if (!this.isModified('password')) {
+            return next();
+        }
+        const hashed = bcrypt.hash(this.password, 10);
+        this.password = hashed;
+        return next();
+        
+    } catch (error) {
+        return next(error);
+    }
+});
+
+userSchema.methods.comparePasswords = async function (attempt, next) {
+    
+}
 
 module.exports = mongoose.model('User', userSchema);
--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true }).then(console.log("DB connected"));
 
 module.exports.User = require('./User');
 module.exports.Poll = require('./Poll');
