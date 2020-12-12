77b8e8ca7991a33ba6925142b4481bef4f567b70
Shubham Singhal <shubham.011190@gmail.com>
2020-12-12 14:23:56 +0530

    Error handler

--- server/index.js
+++ server/index.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const app = express();
+const handle = require('./handlers');
 
 const PORT = 5000;
 app.get('/', (req, res) => {
     res.json({ hello: "world" });
 });
 
-app.use((req, res, next) => {
-    const err = new Error("Not Found");
-    err.status = 404;
-    next(err);
-});
+app.use(handle.notFound);
 
-app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-        err: err.message || "Something went wrong!"
-    })
-});
+app.use(handle.errors);
+
 
 app.listen(PORT, console.log(`Server started at port ${PORT}`));
